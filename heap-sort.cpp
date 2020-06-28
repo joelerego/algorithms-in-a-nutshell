4a387f815846b402e2a440c159e830fe6fa69da9
Joel E. Rego <joelerego@gmail.com>
2020-06-28 23:51:21 +0530

    Added method to build max heap

--- heap-sort.cpp
+++ heap-sort.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-
-int size;
+using namespace std;
 
 // The following functions are just inline functions
 // because they just return the index of the left child,
@@ -13,38 +12,69 @@ inline int rightindex(int i) {
     return i * 2 + 2;
 }
 inline int parentindex(int i) {
-    return (i - 2) / 2;
+    return i / 2;
 }
 
-// The following functions are also inline functions.
-// The return a boolean value depending on the existence
-// of the left child, the right child.
+// The following function maintains the max heap property of the 
+// array. It ensures that for any given index, the children always
+// have a lesser value. If not, the element at that index is "bubbled-down".
 //
-inline boolean hasleftchild(int i) {
-    if(leftindex(int i) < size) return true;
-    else return false; 
-}
-inline boolean hasrightchild(int i) {
-    if(rightindex(int i) < size) return true;
-    else return false; 
+void maxheapify(int heap[], int size, int index) {
+    int leftIndex = leftindex(index);
+    int rightIndex = rightindex(index);
+    
+    int largest = index;
+    if (leftIndex < size and heap[leftIndex] > heap[index]) {
+        largest = leftIndex;
+    }
+    else if (rightIndex < size and heap[rightIndex] > heap[index]) {
+        largest = rightIndex;
+    } 
+    
+    if(largest != index) {
+        swap(heap[index], heap[largest]);
+        maxheapify(heap, size, largest); 
+    }
 }
 
-void heapsort(int heap[]) {
+// There are two kinds of binary heaps: max-heaps and min-heaps. 
+// In both kinds, the values in the nodes satisfy a heap property, 
+// the specifics of which depend on the kind of heap. For heap sort
+// algorithm, we will be using max heap.
+//
+void buildheap(int heap[], int size) {
+    for(int i = size/2; i >= 0; --i) {
+        maxheapify(heap, size, i);
+    }
+}
+
+// The following function performs the heap sorting algorithm.
+//
+void heapsort(int heap[], int size) {    
+    cout << "Inside heap-sort: ";
+    for (int i = 0; i < size; ++i) cout << heap[i] << " ";
+    cout << endl;    
     // @TODO
 }
 
 int main() {
-    int heap = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int heap[] = {9, 4, 8, 10, 6, 5, 7, 3, 2, 1};
+    int size = 10;
     
     cout << "Before heap-sort: ";
     for (auto i : heap) cout << i << " ";
-    cout endl;    
+    cout << endl;    
     
-    heapsort(heap);
+    buildheap(heap, size);
+    cout << "After building max-heap: ";
+    for (auto i : heap) cout << i << " ";
+    cout << endl;  
+    
+    //heapsort(heap, size);
     
     cout << "After heap-sort: ";
     for (auto i : heap) cout << i << " ";
-    cout endl;
+    cout << endl;
     
     return 0;
 }
